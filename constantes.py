26142a5b9817773ce7168836fa832670c80f8254
Alissonfelipe1234 <Alissonfelipe1234@outlook.com>
2020-03-02 15:23:11 -0300

    update master

---
+++ constantes.py
@@ -0,0 +1,23 @@
+# Dados dos estados
+DADOS_ESTADO_AC = 'data/estados/Dados-AC.csv'
+DADOS_ESTADO_ES = 'data/estados/Dados-ES.csv'
+DADOS_ESTADO_RN = 'data/estados/Dados-RN.csv'
+
+# Microregiões IBGE por ano
+DADOS_MR_NOMES  = 'data/microregioes/microregioes_ibge.csv'
+DADOS_MR_2006   = 'data/microregioes/micro_merged_2006.csv'
+DADOS_MR_2007   = 'data/microregioes/micro_merged_2007.csv'
+DADOS_MR_2008   = 'data/microregioes/micro_merged_2008.csv'
+DADOS_MR_2009   = 'data/microregioes/micro_merged_2009.csv'
+DADOS_MR_2010   = 'data/microregioes/micro_merged_2010.csv'
+DADOS_MR_2011   = 'data/microregioes/micro_merged_2011.csv'
+DADOS_MR_2012   = 'data/microregioes/micro_merged_2012.csv'
+DADOS_MR_2013   = 'data/microregioes/micro_merged_2013.csv'
+DADOS_MR_2014   = 'data/microregioes/micro_merged_2014.csv'
+DADOS_MR_2015   = 'data/microregioes/micro_merged_2015.csv'
+DADOS_MR_2016   = 'data/microregioes/micro_merged_2016.csv'
+
+# Diretórios
+DIR_INDICADORES_UF            = 'data/indicadores/uf'
+DIR_INDICADORES_MICRORREGIOES = 'data/indicadores/microrregioes'
+DIR_INDICADORES_MUNICIPIOS    = 'data/indicadores/municipios'
